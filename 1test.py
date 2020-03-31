667f5de60838d2f8aeeb5779df37988ba8a63fc8
Ryan Pettit <doorz1009@gmail.com>
2020-03-31 13:05:37 -0400

    move over to desktop

--- 1test.py
+++ 1test.py
@@ -12,11 +12,6 @@ from scipy.stats import gamma
 mu, sigma = 1.5, 0.5 # mean and standard deviation
 s = np.random.lognormal(mu, sigma, 1000)
 n = len(s)
-arrival_count = 189
-order_count = 188
-payment_count = 184
-pickup_count = 129
-count_arr = [arrival_count, order_count, payment_count, pickup_count]
 
 def get_data(filename, col):
     with open(filename, newline='') as csvfile:
@@ -56,7 +51,7 @@ stat_arr = []
 figure, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
 figures = [ax1, ax2, ax3, ax4]
 current_fig = 0
-counts = []
+
 for plot in plots:
     c_plt = figures[current_fig] # assign current plot
     if plot == plots[0]:
@@ -64,26 +59,15 @@ for plot in plots:
         plot.sort()
 
         # Plot the histogram
-        count, bins, ignored = c_plt.hist(plot, int(len(plot)**(1/2)*2), density=False, align='mid', color='b')
-        counts.append(count)
-        count, bins, ignored = c_plt.hist(plot, int(len(plot)**(1/2)*2), density=True, align='mid', color='b')
+        count, bins, ignored = c_plt.hist(plot, int(len(plot)**(1/2)*2), density=True, align='mid')
         stat_arr.append([count, bins, ignored])
 
         # Make a PDF
         x = np.linspace(min(bins), max(bins), 10000)
-        estimator = len(plot)/np.sum(plot)
+        estimator = len(plot) / np.sum(plot)
+        estimator = 1 / np.average(plot)
         pdf = (estimator * np.exp(-(estimator * x)))
 
-        expected_vals = []
-        for i in range(len(bins)):
-            if i < (len(bins) - 1):
-                mp = (bins[i+1] + bins[i]) / 2 # Calculate midpoint of histogram box
-                #expected = (estimator * np.exp(-(estimator * mp)))
-                expected_vals.append(estimator * np.exp(-(estimator * mp)))
-
-        #print(expected_vals - count_std)
-        print(stats.chisquare(count, expected_vals))
-        #print(expected_vals, count_std)
         # Plot the PDF
         c_plt.plot(x, pdf, linewidth=2, color='r')
     else:
@@ -91,32 +75,18 @@ for plot in plots:
         plot.sort()
 
         # Plot the histogram
-        count, bins, ignored = c_plt.hist(plot, int(len(plot)**(1/2)), density=False, align='mid', color='b')
-        counts.append(count)
-        count, bins, ignored = c_plt.hist(plot, int(len(plot)**(1/2)), density=True, align='mid', color='b')
+        count, bins, ignored = c_plt.hist(plot, int(len(plot)**(1/2)*2), density=True, align='mid')
         stat_arr.append([count, bins, ignored])
 
         # Make a PDF
         shape, loc, scale = gamma.fit(order_times)
         x = np.linspace(min(bins), max(bins), 10000)
         pdf = x**(shape-1) * (np.exp(-x/scale) / (sps.gamma(shape) * scale**shape))
-
-        expected_vals = []
-        for i in range(len(bins)):
-            if i < (len(bins) - 1):
-                mp = (bins[i+1] + bins[i]) / 2 # Calculate midpoint of histogram box
-                #expected = (estimator * np.exp(-(estimator * mp)))
-                expected_vals.append(mp**(shape-1) * (np.exp(-mp/scale) / (sps.gamma(shape) * scale**shape)))
-
-        print(stats.chisquare(count, expected_vals))
         # Plot the PDF
         c_plt.plot(x, pdf, linewidth=2, color='r')
 
     current_fig += 1
 
-
-for count in counts:
-    print(count)
 
 # superimpose an exponential distribution pdf
 #x = np.linspace(min(stats[0][1]), max(stats[0][1]), 10000)
@@ -139,22 +109,10 @@ for v in s:
     sum = sum + (np.log(v) - mu)**2
 sigma = math.sqrt(sum / n)
 
-plt.figure()
-count, bins, ignored = plt.hist(s, 20, density=True, align='mid', color='b')
-x = np.linspace(min(bins), max(bins), 10000)
-pdf = (np.exp(-(np.log(x) - mu)**2 / (2 * sigma**2))
-       / (x * sigma * np.sqrt(2 * np.pi)))
-
-expected_vals = []
-for i in range(len(bins)):
-    if i < (len(bins) - 1):
-        mp = (bins[i+1] + bins[i]) / 2 # Calculate midpoint of histogram box
-        #expected = (estimator * np.exp(-(estimator * mp)))
-        expected_vals.append(np.exp(-(np.log(mp) - mu)**2 / (2 * sigma**2)) / (mp * sigma * np.sqrt(2 * np.pi)))
-        #expected_vals.append(mp**(shape-1) * (np.exp(-mp/scale) / (sps.gamma(shape) * scale**shape)))
-print(stats.chisquare(count, expected_vals))
-
-plt.plot(x, pdf, linewidth=2, color='y')
+#x = np.linspace(min(bins), max(bins), 10000)
+#pdf = (np.exp(-(np.log(x) - mu)**2 / (2 * sigma**2))
+       #/ (x * sigma * np.sqrt(2 * np.pi)))
+#plt.plot(x, pdf, linewidth=2, color='y')
 
 
 plt.axis('tight')
